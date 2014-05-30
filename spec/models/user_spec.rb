f8fb00d49a2c425bb296969e3fe36796686ffe8f
Chiaki Sato <chiakiwave@hotmail.com>
2014-05-31 05:09:33 +1000

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,6 @@ describe User do
                      password: "foobar", password_confirmation: "foobar")
   end
 
-
   subject { @user }
 
   it { should respond_to(:name) }
@@ -15,6 +14,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
@@ -109,4 +109,8 @@ describe User do
     end
   end  
 
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 end
